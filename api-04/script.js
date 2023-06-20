8238f0899c8a40f7f879671d346d58760d6a4919
sakibhossain <sakibhossainceo@gmail.com>
2023-06-19 23:21:11 -0700

    added

--- api-04/script.js
+++ api-04/script.js
@@ -1,15 +1,18 @@
-// const loadLink = () => {
-//     fetch('https://api.shrtco.de/v2/shorten?url=https://github.com/ProgrammingHero1?tab=repositories')
-//     .then(res => res.json())
-//     .then(data => console.log(data.result.original_link))
-// }
+const loadLink = () => {
+    fetch('https://api.shrtco.de/v2/shorten?url=https://github.com/ProgrammingHero1?tab=repositories')
+    .then(res => res.json())
+    .then(data => console.log(data.result.original_link))
+}
  
 document.getElementById('link-btn').addEventListener('click', function(){
     const inputLink = document.getElementById('InputLinkField');
-    const inputText = inputLink.value
+    const inputText = inputLink.value;
     console.log(inputText);
+    const showLinkBox =  document.getElementById('show-link');
+    showLinkBox.innerText = inputText;
 })
 
 
 
-// loadLink();
+
+loadLink();
