a15ebd569aec24fbf8dfae792c2c6b611c197605
sakibhossain <sakibhossainceo@gmail.com>
2023-06-20 03:53:42 -0700

    added

--- api-05/script.js
+++ api-05/script.js
@@ -1,16 +1,37 @@
-const loadAdvice = () => {
+const loadRandomAdvice = () => {
     fetch('https://api.adviceslip.com/advice')
     .then(res => res.json())
     .then(data => displayAdvice(data.slip))
 }
 
-const displayAdvice = advice => {
-    console.log(advice);
-    const adviceContainer = document.getElementById('advice-container');
-    adviceContainer.innerHTML =`
-    <p>Advice: ${advice.id}</p>
-        <h3>${advice.advice}</h3>
+const displayAdvice = randomAdvice => {
+    // console.log(advice);
+    const randomadviceContainer = document.getElementById('random-advice');
+    randomadviceContainer.innerHTML =`
+        <p>Advice: ${randomAdvice.id}</p>
+        <h3>${randomAdvice.advice}</h3>
     `
 }
 
-loadAdvice();
+loadRandomAdvice();
+
+const loadAdviceId = (adviceId) => {
+    const url = `https://api.adviceslip.com/advice/${adviceId}`;
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayAdviceId(data.slip))
+}
+
+const displayAdviceId = idAdvice => {
+    const inputIdField = document.getElementById('input-id');
+    const adviceId = inputIdField.value;
+    loadAdviceId(adviceId);
+    const idAdviceContainer = document.getElementById('id-advice');
+    idAdviceContainer.innerHTML= `
+        <p class="fs-4">Advice: ${idAdvice.id}</p>
+        <h3>${idAdvice.advice}</h3>
+    `
+
+}
+
+loadAdviceId('1');
